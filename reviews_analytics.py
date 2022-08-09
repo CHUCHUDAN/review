e434bf7cde9a3637474c243e7f91e5a0f9dfd03f
daniel <daniel870217@gmail.com>
2022-08-09 12:57:02 +0800

    first commit

---
+++ reviews_analytics.py
@@ -0,0 +1,70 @@
+data=[]
+count = 0
+with open("reviews.txt","r") as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+# print("檔案讀取完了,總共有",len(data),"筆資料")
+# print(data[0])
+
+
+
+sum_len = 0
+for d in data:
+	n = len(d)
+	sum_len += n
+		
+print("每筆平均資料長度是", sum_len/len(data))
+
+
+	
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
+print("一共有", len(new), "筆小於100的資料")
+
+good = []
+for d in data:
+	if "good" in d:
+		good.append(d)
+
+print(len(good))
+
+
+
+wc = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+			
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] >= 1000000:
+		print(word,wc[word])
+
+print(len(wc))
+print(wc["Daniel"])
+
+while True:
+	word = input("想查詢甚麼字?: ")
+	if word == "q":
+		break
+	if word in wc:
+		print(word,"出現過",wc[word],"次")
+	else:
+		print("資料中沒這個字")
+
+print("感謝您使用本查詢系統")
+
+
+
+
+
